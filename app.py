c21ee0c05e75881ec2757cec18fb886a2c70fa64
Абдельнур Алекс Фатех <a.aabdelnur@mail.ru>
2023-10-21 06:30:44 +0300

    len' pisat' nazvanie

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+
+from models.Group import Group
+from models.Lesson import Lesson
+from settings import *
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return "hello"
+@app.get("/get_lessons")
+def get_lessons():
+    groups = session.query(Group).all()
+    resp = {}
 
+    for group in groups:
+        lessons = session.query(Lesson).filter_by(group=group.id).all()
+        for l in lessons:
+            pass
+        resp[group.name] = None
+    return resp
+
+
+if __name__ == "__main__":
+    os.system("uvicorn app:app")
